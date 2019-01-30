3ad2ceb2e264240a52e1e396c2b7d702ecf88dde
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-01-30 09:46:02 +0200

    connection set up

--- config/Database.php
+++ config/Database.php
@@ -1,12 +1,15 @@
 <?php
 class Database
 {
+    private $isLocal = true;
     public function connect()
     {
         $conn = null;
         try {
-            //$conn = new PDO('mysql:host=127.0.0.1;dbname=ndusys0_student_io_db','ndusys0_dok','Harder01!');
-            $conn = new PDO('mysql:host=localhost;dbname=ndusys0_student_io_db', 'root', '');
+            if ($this->isLocal) {
+                $conn = new PDO('mysql:host=localhost;dbname=ndusys0_student_io_db', 'root', '');
+            }
+            $conn = new PDO('mysql:host=127.0.0.1;dbname=ndusys0_student_io_db', 'ndusys0_dok', 'Harder01!');
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo json_encode($e->getMessage()());
